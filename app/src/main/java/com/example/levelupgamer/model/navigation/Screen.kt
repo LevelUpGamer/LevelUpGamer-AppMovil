3e4eee13e0c15aaef9a164120b682dae4bbb1c00
AranOpazo <ara.opazo@duocuc.cl>
2025-10-15 17:57:29 -0300

    feat(nav): agrega Screen y NavigationEvent en model/navigation

---
+++ app/src/main/java/com/example/levelupgamer/model/navigation/Screen.kt
@@ -0,0 +1,13 @@
+package com.example.levelupgamer.model.navigation
+
+sealed class Screen(val route: String) {
+    data object Home: Screen (route = "home_page")
+    data object Profile: Screen (route = "profile_page")
+    data object Catalog: Screen (route = "catalog_page")
+
+    data class Detail (val itemId: String) : Screen(route = "detail_page/{itemId}"){
+        fun buidRoute():String {
+            return route.replace("{itemId}", itemId)
+        }
+    }
+}
