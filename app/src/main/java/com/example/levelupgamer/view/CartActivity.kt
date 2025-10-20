ee6ced35689ab0a8513f3159907d1e612120b133
IsaOfViltrum <is.sanchezy@duocuc.cl>
2025-10-20 12:24:11 -0300

    Menu en carrito de compras

--- app/src/main/java/com/example/levelupgamer/view/CartActivity.kt
+++ app/src/main/java/com/example/levelupgamer/view/CartActivity.kt
@@ -1,13 +1,62 @@
 package com.example.levelupgamer.view
-
+import androidx.drawerlayout.widget.DrawerLayout
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import com.example.levelupgamer.CatalogoActivity
 import com.example.levelupgamer.R
+import com.google.android.material.navigation.NavigationView
 
 class CartActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_carrito)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.navigation_view)
+
+        // Toolbar
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = ""
+
+        // Botón hamburguesa manual
+        val btnMenu: ImageButton = findViewById(R.id.btnMenu)
+        btnMenu.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        // Click en items del menú lateral
+        navView.setNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }
+
+                R.id.nav_catalogo -> {
+                    val intent = Intent(this, CatalogoActivity::class.java)
+                    startActivity(intent)
+                }
+
+                R.id.nav_carrito -> {
+                    val intent = Intent(this, CartActivity::class.java)
+                    startActivity(intent)
+                }
+
+                R.id.nav_cerrar_sesion -> {
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+            drawerLayout.closeDrawers()
+            true
+        }
     }
 }
