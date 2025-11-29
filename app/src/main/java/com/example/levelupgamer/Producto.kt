ef93a60b3b56cd0d1a88bd94307c7a61e02b710f
AranOpazo <ara.opazo@duocuc.cl>
2025-11-29 20:21:33 -0300

    Se eliminan comentarios

--- app/src/main/java/com/example/levelupgamer/Producto.kt
+++ app/src/main/java/com/example/levelupgamer/Producto.kt
@@ -6,28 +6,3 @@ data class Producto(
     val precio: Double,
     val imagenResId: Int // Para poder mostrar la imagen en el carrito
 )
-
-//object CarritoManager {
-//    // Lista para almacenar los productos. Usamos 'mutableListOf' para poder añadir y quitar.
-//    private val _items = mutableListOf<Producto>()
-//
-//    // Propiedad pública que devuelve una copia inmutable.
-//    val items: List<Producto>
-//        get() = _items.toList()
-//
-//    fun agregarProducto(producto: Producto) {
-//        _items.add(producto)
-//    }
-//
-//    fun eliminarProducto(producto: Producto) {
-//        _items.remove(producto)
-//    }
-//
-//    fun vaciarCarrito() {
-//        _items.clear()
-//    }
-//
-//    fun calcularTotal(): Double {
-//        return _items.sumOf { it.precio }
-//    }
-//}
