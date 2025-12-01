68c60f363e3438c56deb8f5c04608e3bef4d0e4a
AranOpazo <ara.opazo@duocuc.cl>
2025-11-30 22:19:45 -0300

    Se modifican dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,44 +51,42 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
 
+    implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.recyclerview:recyclerview:1.4.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("com.google.android.material:material:1.13.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.cardview:cardview:1.0.0")
+
+    // Navegación y splash
+    implementation("androidx.navigation:navigation-compose:2.8.0")
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+
+    // Imágenes en Compose
+    implementation("io.coil-kt:coil-compose:2.7.0")
+
+    // TESTS
     testImplementation(libs.junit)
 
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 
-    implementation("androidx.recyclerview:recyclerview:1.4.0")
-    implementation ("androidx.appcompat:appcompat:1.7.1")
-    implementation("com.google.android.material:material:1.13.0")
-    implementation("androidx.compose.material:material-icons-extended")
-    implementation("androidx.core:core-splashscreen:1.0.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.cardview:cardview:1.0.0")
-    implementation("androidx.navigation:navigation-compose:2.8.0")
-
-    // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
-    // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
-    // Imágenes en Compose
-    implementation("io.coil-kt:coil-compose:2.7.0")
-
-<<<<<<< HEAD
-
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-=======
-    // Pruebas unitarias
     // Tests de corrutinas
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
     // MockK (mocks en Kotlin)
     testImplementation("io.mockk:mockk:1.13.13")
-    // MockWebServer (simular API MmoBomb)
+    // MockWebServer (simular APIs)
     testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
->>>>>>> f9d242c36d9c2aa48e23c0f8c9827dfb167a6552
 }
